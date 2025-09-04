98454794456595568604e9c331f0ab402e2f3932
lmorocoramos <lmorocoramos@gmail.com>
2025-09-04 09:50:59 -0500

    fix: specify algorithm. Add resource server params.

--- infrastructure/entry-points/reactive-web/src/main/java/com/crediya/auth/api/security/JwtProvider.java
+++ infrastructure/entry-points/reactive-web/src/main/java/com/crediya/auth/api/security/JwtProvider.java
@@ -4,9 +4,7 @@ import com.crediya.auth.model.user.User;
 import io.jsonwebtoken.*;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
-import io.jsonwebtoken.security.SignatureException;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
 import javax.crypto.SecretKey;
@@ -23,54 +21,12 @@ public class JwtProvider implements com.crediya.auth.model.user.gateways.JwtProv
   @Value("${jwt.expiration}")
   private Integer expiration;
 
-  public String generateToken(UserDetails userDetails) {
-    return Jwts.builder()
-      .subject(userDetails.getUsername())
-      .claim("roles", userDetails.getAuthorities())
-      .issuedAt(new Date())
-      .expiration(new Date(new Date().getTime() + expiration))
-      .signWith(getKey(secret))
-      .compact();
-  }
-
   public Claims getClaims(String token) {
     return Jwts.parser()
       .verifyWith(getKey(secret))
       .build()
       .parseSignedClaims(token)
       .getPayload();
-  }
-
-  public String getSubject(String token) {
-    return Jwts.parser()
-      .verifyWith(getKey(secret))
-      .build()
-      .parseSignedClaims(token)
-      .getPayload()
-      .getSubject();
-  }
-
-  public boolean validate(String token){
-    try {
-      Jwts.parser()
-        .verifyWith(getKey(secret))
-        .build()
-        .parseSignedClaims(token)
-        .getPayload()
-        .getSubject();
-      return true;
-    } catch (ExpiredJwtException e) {
-      LOGGER.severe("token expired");
-    } catch (UnsupportedJwtException e) {
-      LOGGER.severe("token unsupported");
-    } catch (MalformedJwtException e) {
-      LOGGER.severe("token malformed");
-    } catch (SignatureException e) {
-      LOGGER.severe("bad signature");
-    } catch (IllegalArgumentException e) {
-      LOGGER.severe("illegal args");
-    }
-    return false;
   }
 
   private SecretKey getKey(String secret) {
@@ -86,7 +42,7 @@ public class JwtProvider implements com.crediya.auth.model.user.gateways.JwtProv
       .claim("role", user.getUserRole().name())
       .setIssuedAt(new Date())
       .setExpiration(new Date(System.currentTimeMillis() + expiration))
-      .signWith(getKey(secret))
+      .signWith(getKey(secret), SignatureAlgorithm.HS256)
       .compact();
   }
 }
