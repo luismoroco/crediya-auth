640164a2de3ffa6d92a8e67dbc9f66aafc7ed16d
lmorocoramos <lmorocoramos@gmail.com>
2025-09-04 14:38:54 -0500

    chore: remove unused files.

--- infrastructure/entry-points/reactive-web/src/main/java/com/crediya/auth/api/config/SwaggerConfig.java
+++ infrastructure/entry-points/reactive-web/src/main/java/com/crediya/auth/api/config/SwaggerConfig.java
@@ -4,7 +4,6 @@ import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -33,7 +32,6 @@ public class SwaggerConfig {
           .bearerFormat("JWT")
           .description("JWT Authentication")
       ))
-      //.addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
       .info(new io.swagger.v3.oas.models.info.Info()
         .title("Authentication-Service")
         .version("1.0")
